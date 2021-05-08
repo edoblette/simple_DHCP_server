8d63968462818add2e90a5d8f0cf18531f9468ea
edoblette <edwardoblette@gmail.com>
2021-05-09 01:35:57 +0200

    DHCP dataframe transmission OK, Discover OK, Offer OK, Request OK, Ack OK. Need to add some option( randomize XID, add time stamp)

--- dhcp_client.py
+++ dhcp_client.py
@@ -3,7 +3,7 @@ import socket, uuid
 MAX_BYTES = 1024
 serverPort = 67
 clientPort = 68
-
+raw_mac_addr = uuid.getnode()
 
 class DHCP_client(object):
     def client(self):
@@ -16,60 +16,101 @@ class DHCP_client(object):
         print("Send DHCP discovery.")
         data = DHCP_client.discover_get();
         clientSocket.sendto(data, dest)
-        
-        data, address = clientSocket.recvfrom(MAX_BYTES)
+        print(data)
+
+        packet, address = clientSocket.recvfrom(MAX_BYTES)
         print("Receive DHCP offer.")
+        print(DHCP_client.packet_analyser(packet))
+        xid, yiaddr, siaddr, chaddr, magic_cookie = DHCP_client.packet_analyser(packet)[4], DHCP_client.packet_analyser(packet)[8], DHCP_client.packet_analyser(packet)[9], DHCP_client.packet_analyser(packet)[11], DHCP_client.packet_analyser(packet)[12]
+        print("YIADDR : " + DHCP_client.ip_addr_format(DHCP_client.packet_analyser(packet)[8]))
+        print("SIADDR : " + DHCP_client.ip_addr_format(DHCP_client.packet_analyser(packet)[9]))
+        print("option : " + '{} {} {} {} {} {} {} {} {} {} {} {} {} {} {} {} {} {} {}'.format(*bytearray(DHCP_client.packet_analyser(packet)[13])))
+
+        print("Send DHCP request.")
+        data = DHCP_client.request_get(xid, yiaddr, siaddr, chaddr, magic_cookie);
+        clientSocket.sendto(data, dest)
+        print(data)
+
+        data,address = clientSocket.recvfrom(MAX_BYTES)
+        print("Receive DHCP pack.\n")
         #print(data)
+
+        
+
+    def ip_addr_format(yiaddr):
+        yiaddr = '{}.{}.{}.{}'.format(*bytearray(yiaddr))
+
+        return yiaddr
+
+    def packet_analyser(packet): #avec cette méthode on récupère le message discover d'un client
+        OP = packet[0]
+        HTYPE = packet[1]
+        HLEN = packet[2]
+        HOPS = packet[3]
+        XID = packet[4:8]
+        SECS = packet[8:10]
+        FLAGS = packet[10:12]
+        CIADDR = packet[12:16]
+        YIADDR = packet[16:20]
+        SIADDR = packet[20:24]
+        GIADDR = packet[24:28]
+        CHADDR = packet[28:28 + 16 +  192]
+        magic_cookie = packet[236:240]
+        DHCPoptions = packet[240:]
+
+        return OP, HTYPE, HLEN, HOPS, XID, SECS, FLAGS, CIADDR, YIADDR, SIADDR, GIADDR, CHADDR, magic_cookie, DHCPoptions
 
     def discover_get():
         OP = bytes([0x01])
         HTYPE = bytes([0x01])
         HLEN = bytes([0x06])
         HOPS = bytes([0x00])
-        XID = bytes([0x39, 0x03, 0xF3, 0x26])
+        XID = bytes([0x39, 0x03, 0xF3, 0x26]) #a randomiser
         SECS = bytes([0x00, 0x00])
         FLAGS = bytes([0x00, 0x00])
         CIADDR = bytes([0x00, 0x00, 0x00, 0x00])
         YIADDR = bytes([0x00, 0x00, 0x00, 0x00])
         SIADDR = bytes([0x00, 0x00, 0x00, 0x00])
         GIADDR = bytes([0x00, 0x00, 0x00, 0x00]) 
-        CHADDR1 = bytes.fromhex(hex(uuid.getnode())[2:])[1:] #remplacer par argv
-        CHADDR2 = bytes([0x8D, 0x59, 0x00, 0x00]) 
+        CHADDR1 = bytes.fromhex(hex(raw_mac_addr)[2:10]) #remplacer par argv  (4 octets)
+        CHADDR2 = bytes.fromhex(hex(raw_mac_addr)[10:14]) + bytes([0x00, 0x00]) #(2 octets + 2 octet de bourrage)
         CHADDR3 = bytes([0x00, 0x00, 0x00, 0x00]) 
         CHADDR4 = bytes([0x00, 0x00, 0x00, 0x00]) 
         CHADDR5 = bytes(192)
         Magiccookie = bytes([0x63, 0x82, 0x53, 0x63])
         DHCPOptions1 = bytes([53 , 1 , 1])
         DHCPOptions2 = bytes([50 , 4 , 0xC0, 0xA8, 0x01, 0x64])
+        ENDMARK = bytes([0xff])
 
-        print("adresse mac est :" + str(CHADDR1)) 
-        package = OP + HTYPE + HLEN + HOPS + XID + SECS + FLAGS + CIADDR +YIADDR + SIADDR + GIADDR + CHADDR1 + CHADDR2 + CHADDR3 + CHADDR4 + CHADDR5 + Magiccookie + DHCPOptions1 + DHCPOptions2
+        print("adresse mac est :")
+        chaddr = hex(raw_mac_addr)[2:]
+        mac_addr = ':'.join(chaddr[i:i+2] for i in range(0,12,2))
+        print(mac_addr) 
+
+        package = OP + HTYPE + HLEN + HOPS + XID + SECS + FLAGS + CIADDR + YIADDR + SIADDR + GIADDR + CHADDR1 + CHADDR2 + CHADDR3 + CHADDR4 + CHADDR5 + Magiccookie + DHCPOptions1 + DHCPOptions2 + ENDMARK
 
         return package
 
-    def request_get():
+    def request_get(xid, yiaddr, siaddr, chaddr, magic_cookie):
         OP = bytes([0x01])
         HTYPE = bytes([0x01])
         HLEN = bytes([0x06])
         HOPS = bytes([0x00])
-        XID = bytes([0x39, 0x03, 0xF3, 0x26])
+        XID = xid
         SECS = bytes([0x00, 0x00])
         FLAGS = bytes([0x00, 0x00])
-        CIADDR = bytes([0x00, 0x00, 0x00, 0x00])
+        CIADDR = yiaddr
         YIADDR = bytes([0x00, 0x00, 0x00, 0x00])
-        SIADDR = bytes([0x00, 0x00, 0x00, 0x00])
+        SIADDR = siaddr
         GIADDR = bytes([0x00, 0x00, 0x00, 0x00])
-        CHADDR1 = bytes.fromhex(hex(uuid.getnode())[2:])[1:] #remplacer par argv
-        CHADDR2 = bytes([0x5C, 0xA7, 0x00, 0x00])
-        CHADDR3 = bytes([0x00, 0x00, 0x00, 0x00]) 
-        CHADDR4 = bytes([0x00, 0x00, 0x00, 0x00]) 
-        CHADDR5 = bytes(192)
-        Magiccookie = bytes([0x63, 0x82, 0x53, 0x63])
+        CHADDR = chaddr
+        Magiccookie = magic_cookie
         DHCPOptions1 = bytes([53 , 1 , 3])
-        DHCPOptions2 = bytes([50 , 4 , 0xC0, 0xA8, 0x01, 0x64])
+        DHCPOptions2 = bytes([50 , 4 ]) + yiaddr
         DHCPOptions3 = bytes([54 , 4 , 0xC0, 0xA8, 0x01, 0x01])
-	
-        package = OP + HTYPE + HLEN + HOPS + XID + SECS + FLAGS + CIADDR +YIADDR + SIADDR + GIADDR + CHADDR1 + CHADDR2 + CHADDR3 + CHADDR4 + CHADDR5 + Magiccookie + DHCPOptions1 + DHCPOptions2 +  DHCPOptions3
+        ENDMARK = bytes([0xff])
+
+        package = OP + HTYPE + HLEN + HOPS + XID + SECS + FLAGS + CIADDR + YIADDR + SIADDR + GIADDR + CHADDR  + Magiccookie + DHCPOptions1 + DHCPOptions2 +  DHCPOptions3 + ENDMARK
 
         return package
 
--- dhcp_server.py
+++ dhcp_server.py
@@ -1,14 +1,23 @@
 from socket import *
+import re
 
+MAX_BYTES = 1024
 serverPort = 67
 clientPort = 68
+lease_time = 300
 
 class serverDHCP(object):
 
-	network = '192.168.65.0'
+	ip_list = '192.168.65.100'
 	router = '192.168.65.1'
 
-	def get_clientmsg(packet): #avec cette méthode on récupère le message discover d'un client
+	def mac_addr_format(chaddr):
+		chaddr = chaddr.hex()[:16]
+		mac_addr = ':'.join(chaddr[i:i+2] for i in range(0,12,2))
+
+		return mac_addr
+
+	def packet_analyser(packet): #avec cette méthode on récupère le message discover d'un client
 		OP = packet[0]
 		HTYPE = packet[1]
 		HLEN = packet[2]
@@ -20,7 +29,7 @@ class serverDHCP(object):
 		YIADDR = packet[16:20]
 		SIADDR = packet[20:24]
 		GIADDR = packet[24:28]
-		CHADDR = packet[28:28 + HLEN + 192]
+		CHADDR = packet[28:28 + 16 + 192]
 		magic_cookie = packet[236:240]
 		DHCPoptions = packet[240:]
 
@@ -34,21 +43,45 @@ class serverDHCP(object):
 		XID = xid
 		SECS = bytes([0x00, 0x00])
 		FLAGS = bytes([0x00, 0x00])
-		CIADDR = ciaddr
-		YIADDR = inet_aton(serverDHCP.network)
+		CIADDR = ciaddr 
+		YIADDR = inet_aton(serverDHCP.ip_list) #adresse a donner
 		SIADDR = inet_aton(serverDHCP.router)
 		GIADDR = bytes([0x00, 0x00, 0x00, 0x00])
 		CHADDR = chaddr
-		magic_cookie = magicookie
-		DHCPoptions1 = bytes([53, 1, 2])
-		DHCPoptions2 = bytes([1 , 4 , 0xFF, 0xFF, 0xFF, 0x00])
-		DHCPoptions3 = bytes([3 , 4 , 0xC0, 0xA8, 0x01, 0x01])
-		DHCPOptions4 = bytes([51 , 4 , 0x00, 0x01, 0x51, 0x80])
-		DHCPOptions5 = bytes([54 , 4 , 0xC0, 0xA8, 0x01, 0x01])
+		Magiccookie = magicookie
+		DHCPoptions1 = bytes([53, 1, 2])  # DHCP Offer
+		DHCPoptions2 = bytes([1 , 4 , 0xFF, 0xFF, 0xFF, 0x00]) # subnet_mask 255.255.255.0
+		DHCPoptions3 = bytes([3 , 4 ]) + inet_aton(router) # router
+		DHCPOptions4 = bytes([51 , 4]) + ((lease_time).to_bytes(4, byteorder='big')) #86400s(1, day) IP address lease time
+		DHCPOptions5 = bytes([54 , 4 , 0xC0, 0xA8, 0x01, 0x01]) # DHCP server
+		ENDMARK = bytes([0xff])
 
-		package = OP + HTYPE + HLEN + HOPS + XID + SECS + FLAGS + CIADDR + YIADDR + SIADDR + GIADDR + CHADDR + magic_cookie + DHCPoptions1 + DHCPoptions2 + DHCPoptions3 + DHCPOptions4 + DHCPOptions5
+		package = OP + HTYPE + HLEN + HOPS + XID + SECS + FLAGS + CIADDR + YIADDR + SIADDR + GIADDR + CHADDR + Magiccookie + DHCPoptions1 + DHCPoptions2 + DHCPoptions3 + DHCPOptions4 + DHCPOptions5 + ENDMARK
 		return package
 
+	def pack_get(xid, ciaddr, chaddr, magicookie, router):
+		OP = bytes([0x02])
+		HTYPE = bytes([0x01])
+		HLEN = bytes([0x06])
+		HOPS = bytes([0x00])
+		XID = xid
+		SECS = bytes([0x00, 0x00])
+		FLAGS = bytes([0x00, 0x00])
+		CIADDR = ciaddr 
+		YIADDR = inet_aton(serverDHCP.ip_list) #adresse a donner
+		SIADDR = inet_aton(serverDHCP.router)
+		GIADDR = bytes([0x00, 0x00, 0x00, 0x00])
+		CHADDR = chaddr
+		Magiccookie = magicookie
+		DHCPoptions1 = bytes([53 , 1 , 5]) #DHCP ACK(value = 5)
+		DHCPoptions2 = bytes([1 , 4 , 0xFF, 0xFF, 0xFF, 0x00]) #255.255.255.0 subnet mask
+		DHCPoptions3 = bytes([3 , 4 ]) + inet_aton(router)
+		DHCPoptions4 = bytes([51 , 4]) + ((lease_time).to_bytes(4, byteorder='big')) #86400s(1, day) IP address lease time
+		DHCPoptions5 = bytes([54 , 4 , 0xC0, 0xA8, 0x01, 0x01]) #DHCP server
+		ENDMARK = bytes([0xff])
+
+		package = OP + HTYPE + HLEN + HOPS + XID + SECS + FLAGS + CIADDR + YIADDR + SIADDR + GIADDR + CHADDR + Magiccookie + DHCPoptions1 + DHCPoptions2 + DHCPoptions3 + DHCPoptions4 + DHCPoptions5 + ENDMARK
+		return package
 
 
 	def server(self):
@@ -56,7 +89,6 @@ class serverDHCP(object):
 		broadcast_address = '255.255.255.255'
 		subnet_mask = '255.255.255.0'
 		dest = ('<broadcast>', clientPort)
-		lease_time = 300
 
 		server = socket(AF_INET, SOCK_DGRAM)
 		server.setsockopt(SOL_IP, SO_REUSEADDR, 1)
@@ -65,15 +97,29 @@ class serverDHCP(object):
 
 		while True:
 			print("Waiting for DHCP discovery")
-			packet, address = server.recvfrom(4096)
-			xid, ciaddr, chaddr, magic_cookie = serverDHCP.get_clientmsg(packet)[4], serverDHCP.get_clientmsg(packet)[7], serverDHCP.get_clientmsg(packet)[11], serverDHCP.get_clientmsg(packet)[12]
-			#print(packet)
+			packet, address = server.recvfrom(MAX_BYTES)
+			xid, ciaddr, chaddr, magic_cookie = serverDHCP.packet_analyser(packet)[4], serverDHCP.packet_analyser(packet)[7], serverDHCP.packet_analyser(packet)[11], serverDHCP.packet_analyser(packet)[12]
+			print(packet)
+			print("user : " + serverDHCP.mac_addr_format(chaddr)  + " want ip adress")
 
 			print("Received DHCP discovery!")
 			data = serverDHCP.set_offer(xid, ciaddr, chaddr, magic_cookie, serverDHCP.router)
-			print(data)
+			print(magic_cookie)
 			server.sendto(data, dest)
-			print("Waiting for Request...")
+
+			while 1:
+					try:
+						print("Wait DHCP request.")
+						data, address = server.recvfrom(MAX_BYTES)
+						print("Receive DHCP request.")
+						#print(data)
+
+						print("Send DHCP pack.\n")
+						data = serverDHCP.pack_get(xid, ciaddr, chaddr, magic_cookie, serverDHCP.router)
+						server.sendto(data, dest)
+						break
+					except:
+						raise
 
 
 if __name__ == '__main__':
